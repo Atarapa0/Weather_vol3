ca42ba91eb790ba35e1598b6dd5f83d25d4e327a
Furkan Erdoğan <furkaan.er@outlook.com>
2024-11-30 16:52:41 +0300

    Connect Api

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,6 +8,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String apikey="26475ac066314d0baa3130732243011";
+  String city="London";
+  String weatherIcon="";
+  int temperature=0;
+  int windSpeed=0;
+  int humidity=0;
+  int cloud=0;
+  String currentDate="";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +24,7 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Weather Vol3 '),
       ),
       body: const Center(
-        child: Text('Hello, World!'),
+        child: Text('Hello, World!aa'),
       ),
     );
   }
