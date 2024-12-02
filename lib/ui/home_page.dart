b3f1b54bcf989c01fe141b51dd02cfd0e5a173f5
Furkan Erdoğan <furkaan.er@outlook.com>
2024-12-03 00:47:12 +0300

    Upgrade

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:weathervol3/constants.dart';
 //import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -14,75 +15,74 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _cityController = TextEditingController();
   final Constants _constants = Constants();
   static const String apiKey = "b52a2457f3794d5c935191632240112";
-  static  String location="London";
-  String weatherIcon="";
-  int temperature=0;
-  int windSpeed=0;
-  int humidity=0;
-  int cloud=0;
-  String currentDate="";
+  static String location = "London";
+  String weatherIcon = "";
+  int temperature = 0;
+  int windSpeed = 0;
+  int humidity = 0;
+  int cloud = 0;
+  String currentDate = "";
 
-  List houryWeatherforecast=[];
-  List dailyWeatherForecast=[];
+  List houryWeatherforecast = [];
+  List dailyWeatherForecast = [];
 
-  String currentWeatherstatus="";
+  String currentWeatherstatus = "";
 
   //API Call
-  String searchWeatherApi="https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$location&aqi=no";
-
+  String searchWeatherApi =
+      "https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$location&aqi=no";
 
   Future<void> fetchWeatherData(String searchText) async {
-    try{
-      var searchResult=await http.get(Uri.parse(searchWeatherApi +searchText));
-      final weatherData=  Map<String,dynamic>.from(
+    try {
+      var searchResult =
+          await http.get(Uri.parse(searchWeatherApi + searchText));
+      final weatherData = Map<String, dynamic>.from(
           json.decode(searchResult.body) ?? 'No Data');
 
-      var locationData=weatherData["location"];
-      var currentWeather=weatherData["current"];
+      var locationData = weatherData["location"];
+      var currentWeather = weatherData["current"];
       var forecastData = weatherData["forecast"];
 
-
       setState(() {
-        location =getShortLocationName(locationData["name"]);
-        var parseDate=DateTime.parse(locationData["localtime"].substring(0,10));
-        var newDate=DateFormat('MMMMEEEEd').format(parseDate);
-        currentDate=newDate;
+        location = getShortLocationName(locationData["name"]);
+        var parseDate =
+            DateTime.parse(locationData["localtime"].substring(0, 10));
+        var newDate = DateFormat('MMMMEEEEd').format(parseDate);
+        currentDate = newDate;
 
         //update Weather
-        currentWeatherstatus=currentWeather["condition"]["text"];
+        currentWeatherstatus = currentWeather["condition"]["text"];
         //weatherIcon=currentWeatherstatus.replaceAll('', '').toLowerCase()+".png";
-        weatherIcon="https:${currentWeather["condition"]["icon"]}";
-        temperature=currentWeather["temp_c"].toInt();
-        windSpeed=currentWeather["wind_kph"].toInt();
-        humidity=currentWeather["humidity"].toInt();
-        cloud=currentWeather["cloud"].toInt();
+        weatherIcon = "https:${currentWeather["condition"]["icon"]}";
+        temperature = currentWeather["temp_c"].toInt();
+        windSpeed = currentWeather["wind_kph"].toInt();
+        humidity = currentWeather["humidity"].toInt();
+        cloud = currentWeather["cloud"].toInt();
 
         //forecast data
-        dailyWeatherForecast=forecastData["forecastday"];
-        houryWeatherforecast=dailyWeatherForecast[0]["hour"];
+        dailyWeatherForecast = forecastData["forecastday"];
+        houryWeatherforecast = dailyWeatherForecast[0]["hour"];
 
         //print(dailyWeatherForecast);
-
       });
-    }catch(e){
+    } catch (e) {
       debugPrint("Error : $e");
     }
-
-    }
-
+  }
 
 // function to get the first two words of the location
-  static String  getShortLocationName(String s) {
+  static String getShortLocationName(String s) {
     List<String> wordlist = s.split(" ");
-    if(wordlist.isNotEmpty){
-      if(wordlist.length > 1){
+    if (wordlist.isNotEmpty) {
+      if (wordlist.length > 1) {
         return "${wordlist[0]} ${wordlist[1]}";
       } else {
         return wordlist[0];
       }
-    }else{
+    } else {
       return " ";
     }
   }
@@ -96,7 +96,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
 
     Size size = MediaQuery.of(context).size;
     return Scaffold(
@@ -104,14 +105,14 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         width: size.width,
         height: size.height,
-        padding: const EdgeInsets.only(top:70,left: 10,right: 10),
+        padding: const EdgeInsets.only(top: 70, left: 10, right: 10),
         color: _constants.primaryColor.withOpacity(.1),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-              height: size.height*.7,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              height: size.height * .7,
               decoration: BoxDecoration(
                 gradient: _constants.linearGradientBlue,
                 boxShadow: [
@@ -124,11 +125,118 @@ class _HomePageState extends State<HomePage> {
                 ],
                 borderRadius: BorderRadius.circular(20),
               ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/menu.png",
+                      width: 40,
+                      height: 40,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/pin.png",
+                          width: 20,
+                        ),
+                        const SizedBox(
+                          width: 2,
+                        ),
+                        Text(
+                          location,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            _cityController.clear();
+                            showMaterialModalBottomSheet(
+                                context: context,
+                                builder: (context) => SingleChildScrollView(
+                                      controller:
+                                          ModalScrollController.of(context),
+                                      child: Container(
+                                        height: size.height * .2,
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 20,
+                                          vertical: 10,
+                                        ),
+                                        child: Column(
+                                          children: [
+                                            SizedBox(
+                                              width: 70,
+                                              child: Divider(
+                                                thickness: 3.5,
+                                                color: _constants.primaryColor,
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              height: 10,
+                                            ),
+                                            TextField(
+                                              onChanged: (searchText) {
+                                                fetchWeatherData(searchText);
+                                              },
+                                              controller: _cityController,
+                                              autofocus: true,
+                                              decoration: InputDecoration(
+                                                prefixIcon: Icon(
+                                                  Icons.search,
+                                                  color:
+                                                      _constants.primaryColor,
+                                                ),
+                                                suffixIcon: GestureDetector(
+                                                  onTap: () =>
+                                                      _cityController.clear(),
+                                                  child: Icon(
+                                                    Icons.close,
+                                                    color:
+                                                        _constants.primaryColor,
+                                                  ),
+                                                ),
+                                                hintText:
+                                                    'Search City e.g London',
+                                                focusedBorder:
+                                                    OutlineInputBorder(
+                                                  borderSide: BorderSide(
+                                                    color:
+                                                        _constants.primaryColor,
+                                                    width: 2,
+                                                  ),
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ));
+                          },
+                          icon: const Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                )
+              ],
             )
           ],
         ),
       ),
-
     );
   }
 }
