c6dc0e3a65d67a14ab0ec22e2f24ba9c8347dba7
Furkan Erdoğan <furkaan.er@outlook.com>
2024-12-02 23:59:25 +0300

    Upgrade

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:weathervol3/constants.dart';
 //import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Constants _constants = Constants();
   static const String apiKey = "b52a2457f3794d5c935191632240112";
   static  String location="London";
   String weatherIcon="";
@@ -26,7 +30,7 @@ class _HomePageState extends State<HomePage> {
   String currentWeatherstatus="";
 
   //API Call
-  String searchWeatherApi="https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$location&aqi=no";
+  String searchWeatherApi="https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$location&aqi=no";
 
 
   Future<void> fetchWeatherData(String searchText) async {
@@ -37,19 +41,39 @@ class _HomePageState extends State<HomePage> {
 
       var locationData=weatherData["location"];
       var currentWeather=weatherData["current"];
+      var forecastData = weatherData["forecast"];
+
 
       setState(() {
         location =getShortLocationName(locationData["name"]);
+        var parseDate=DateTime.parse(locationData["localtime"].substring(0,10));
+        var newDate=DateFormat('MMMMEEEEd').format(parseDate);
+        currentDate=newDate;
+
+        //update Weather
+        currentWeatherstatus=currentWeather["condition"]["text"];
+        //weatherIcon=currentWeatherstatus.replaceAll('', '').toLowerCase()+".png";
+        weatherIcon="https:${currentWeather["condition"]["icon"]}";
+        temperature=currentWeather["temp_c"].toInt();
+        windSpeed=currentWeather["wind_kph"].toInt();
+        humidity=currentWeather["humidity"].toInt();
+        cloud=currentWeather["cloud"].toInt();
+
+        //forecast data
+        dailyWeatherForecast=forecastData["forecastday"];
+        houryWeatherforecast=dailyWeatherForecast[0]["hour"];
+
+        //print(dailyWeatherForecast);
 
       });
     }catch(e){
-      debugPrint("Error: $e");
+      debugPrint("Error : $e");
     }
 
     }
 
 
-
+// function to get the first two words of the location
   static String  getShortLocationName(String s) {
     List<String> wordlist = s.split(" ");
     if(wordlist.isNotEmpty){
@@ -72,32 +96,39 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather Vol3'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Location: $location",
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Weather: $currentWeatherstatus",
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Temperature: $temperature°C",
-              style: const TextStyle(fontSize: 18),
-            ),
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
 
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        padding: const EdgeInsets.only(top:70,left: 10,right: 10),
+        color: _constants.primaryColor.withOpacity(.1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              height: size.height*.7,
+              decoration: BoxDecoration(
+                gradient: _constants.linearGradientBlue,
+                boxShadow: [
+                  BoxShadow(
+                    color: _constants.primaryColor.withOpacity(.5),
+                    blurRadius: 7,
+                    spreadRadius: 5,
+                    offset: const Offset(0, 5),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(20),
+              ),
+            )
           ],
         ),
       ),
+
     );
   }
 }
